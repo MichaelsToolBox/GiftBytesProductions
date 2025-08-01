7df7d2ee115ee3406080bfe61648213a8cca39ac
Michael Schenk <103676550+MichaelSchenkDe@users.noreply.github.com>
2025-08-01 13:47:57 +0200

    Update latest section

--- src/components/Latest.tsx
+++ src/components/Latest.tsx
@@ -10,52 +10,42 @@ import * as React from 'react';
 
 const articleInfo = [
   {
-    tag: 'Engineering',
-    title: 'The future of AI in software engineering',
-    description:
-      'Artificial intelligence is revolutionizing software engineering. Explore how AI-driven tools are enhancing development processes and improving software quality.',
+    tag: 'Projects',
+    title: 'New project will be a desktop video game',
+    description: 'We announce that our new project will be a free to play desktop video game that will be available on all operating systems',
     authors: [
-      { name: 'Remy Sharp', avatar: '/static/images/avatar/1.jpg' },
-      { name: 'Travis Howard', avatar: '/static/images/avatar/2.jpg' },
+      { name: 'Michael Schenk, Developer', avatar: '/static/images/avatar/1.jpg' },
     ],
   },
   {
-    tag: 'Product',
-    title: 'Driving growth with user-centric product design',
-    description:
-      'Our user-centric product design approach is driving significant growth. Learn about the strategies we employ to create products that resonate with users.',
-    authors: [{ name: 'Erica Johns', avatar: '/static/images/avatar/6.jpg' }],
+    tag: 'Website',
+    title: 'New website',
+    description: 'We announce that our website is finally live and public',
+    authors: [{ name: 'Michael Schenk, Developer', avatar: '/static/images/avatar/1.jpg' }],
   },
   {
-    tag: 'Design',
-    title: 'Embracing minimalism in modern design',
-    description:
-      'Minimalism is a key trend in modern design. Discover how our design team incorporates minimalist principles to create clean and impactful user experiences.',
-    authors: [{ name: 'Kate Morrison', avatar: '/static/images/avatar/7.jpg' }],
+    tag: 'Projects',
+    title: 'TouchSlider is live',
+    description: 'We announce that that our mobile video game "TouchSlider" is avaible for IOS And Android to download for free',
+    authors: [{ name: 'Michael Schenk, Developer', avatar: '/static/images/avatar/1.jpg' }],
   },
   {
     tag: 'Company',
     title: 'Cultivating a culture of innovation',
-    description:
-      'Innovation is at the heart of our company culture. Learn about the initiatives we have in place to foster creativity and drive groundbreaking solutions.',
-    authors: [{ name: 'Cindy Baker', avatar: '/static/images/avatar/3.jpg' }],
+    description: 'Innovation is at the heart of our company culture. Learn about the initiatives we have in place to foster creativity and drive groundbreaking solutions.',
+    authors: [{ name: 'Michael Schenk, Developer', avatar: '/static/images/avatar/1.jpg' }],
   },
   {
     tag: 'Engineering',
     title: 'Advancing cybersecurity with next-gen solutions',
-    description:
-      'Our next-generation cybersecurity solutions are setting new standards in the industry. Discover how we protect our clients from evolving cyber threats.',
-    authors: [
-      { name: 'Agnes Walker', avatar: '/static/images/avatar/4.jpg' },
-      { name: 'Trevor Henderson', avatar: '/static/images/avatar/5.jpg' },
-    ],
+    description: 'Our next-generation cybersecurity solutions are setting new standards in the industry. Discover how we protect our clients from evolving cyber threats.',
+    authors: [{ name: 'Michael Schenk, Developer', avatar: '/static/images/avatar/1.jpg' }],
   },
   {
     tag: 'Product',
     title: 'Enhancing customer experience through innovation',
-    description:
-      'Our innovative approaches are enhancing customer experience. Learn about the new features and improvements that are delighting our users.',
-    authors: [{ name: 'Travis Howard', avatar: '/static/images/avatar/2.jpg' }],
+    description: 'Our innovative approaches are enhancing customer experience. Learn about the new features and improvements that are delighting our users.',
+    authors: [{ name: 'Michael Schenk, Developer', avatar: '/static/images/avatar/1.jpg' }],
   }
 ];
 
