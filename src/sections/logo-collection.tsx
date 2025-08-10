85886edb44be90aa2adfe18222f20fcd5fa64f5f
Michael Schenk <103676550+MichaelSchenkDe@users.noreply.github.com>
2025-08-10 12:36:59 +0200

    Revert "Update logo row"
    This reverts commit 3e794b43a28f30e9af84d04d91b6be57821f490b.

--- src/sections/logo-collection.tsx
+++ src/sections/logo-collection.tsx
@@ -1,23 +1,35 @@
-import { brandLogoComponents, logoContainerBaseStyle } from '../utils/styles/logo-links';
+import { lightModeLogos, darkModeLogos, logoStyle } from '../utils/styles/logo-links';
+import { useColorScheme } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 
-
 export default function LogoCollection() {
-  const iconSize = 64;
+  const { mode, systemMode } = useColorScheme();
+  let logos;
+  if (mode === 'system') {
+    if (systemMode === 'light') {
+      logos = lightModeLogos;
+    } else {
+      logos = darkModeLogos;
+    }
+  } else if (mode === 'light') {
+    logos = lightModeLogos;
+  } else {
+    logos = darkModeLogos;
+  }
 
   return (
-    <Box id="logoCollection" sx={{ py: 2 }}>
-      <Grid container sx={{ justifyContent: "flex-start", mt: 0, flexWrap: 'nowrap', overflowX: 'auto', overflow: 'auto' }}>
-        {brandLogoComponents.map((logo, index) => (
-          <Grid
-            key={index}
-            component="div"
-            sx={{...logoContainerBaseStyle}}
-          >
-            <logo.component
-              size={iconSize}
-              color={`#${logo.hex}`}
+    <Box id="logoCollection" sx={{ py: 4 }}>
+      <Typography component="p" variant="subtitle2" align="center" sx={{ color: 'text.secondary' }}>
+      </Typography>
+      <Grid container sx={{ justifyContent: 'center', mt: 0.5, opacity: 0.6 }}>
+        {logos.map((logo, index) => (
+          <Grid key={index}>
+            <img
+              src={logo}
+              alt={`Index ${index + 1}`}
+              style={logoStyle}
             />
           </Grid>
         ))}
--- src/utils/styles/logo-links.ts
+++ src/utils/styles/logo-links.ts
@@ -1,18 +1,24 @@
-import { SiUnity, SiUnrealengine, SiAndroid, SiAppstore, SiSteam, SiYoutube} from '@icons-pack/react-simple-icons';
-import * as simpleIcons from 'simple-icons';
-
-export const brandLogoComponents = [
-  { component: SiUnity, name: 'Unity', hex: simpleIcons.siUnity.hex },
-  { component: SiUnrealengine, name: 'Unreal Engine', hex: simpleIcons.siUnrealengine.hex },
-  { component: SiAndroid, name: 'Android', hex: simpleIcons.siAndroid.hex },
-  { component: SiAppstore, name: 'App Store', hex: simpleIcons.siAppstore.hex },
-  { component: SiSteam, name: 'Steam', hex: simpleIcons.siSteam.hex },
-  { component: SiYoutube, name: 'YouTube', hex: simpleIcons.siYoutube.hex },
+export const darkModeLogos = [
+  'https://upload.wikimedia.org/wikipedia/commons/c/c4/Unity_2021.svg',
+  'https://upload.wikimedia.org/wikipedia/commons/d/da/Unreal_Engine_Logo.svg',
+  'https://upload.wikimedia.org/wikipedia/commons/3/3e/Android_logo_2023.svg',
+  'https://upload.wikimedia.org/wikipedia/commons/6/67/App_Store_%28iOS%29.svg',
+  'https://upload.wikimedia.org/wikipedia/commons/8/83/Steam_icon_logo.svg',
+  'https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg',
 ];
 
-export const logoContainerBaseStyle = {
-  width: "150px",
-  height: '90px',
-  margin: '0px 48px',
-  opacity: 0.5,
+export const lightModeLogos = [
+  'https://upload.wikimedia.org/wikipedia/commons/c/c4/Unity_2021.svg',
+  'https://upload.wikimedia.org/wikipedia/commons/d/da/Unreal_Engine_Logo.svg',
+  'https://upload.wikimedia.org/wikipedia/commons/3/3e/Android_logo_2023.svg',
+  'https://upload.wikimedia.org/wikipedia/commons/6/67/App_Store_%28iOS%29.svg',
+  'https://upload.wikimedia.org/wikipedia/commons/8/83/Steam_icon_logo.svg',
+  'https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg',
+];
+
+export const logoStyle = {
+  width: '150px',
+  height: '60px',
+  margin: '0 48px',
+  opacity: 0.7,
 };
