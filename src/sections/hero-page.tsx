d8102a26088e7caa0dd2f2ca2f9aa42cf5143d86
Michael Schenk <103676550+MichaelSchenkDe@users.noreply.github.com>
2025-08-31 11:36:14 +0200

    Update video link to actual youtube video

--- src/sections/hero-page.tsx
+++ src/sections/hero-page.tsx
@@ -12,7 +12,7 @@ export default function Hero() {
   const { t } = useTranslation();
 
   // Will need replacing with new video
-  const videoUrl = 'https://www.youtube.com/watch?v=b4Xdktz6EJQ';
+  const videoUrl = 'https://www.youtube.com/watch?v=8GKI7CidwRo';
 
   return (
     <Box id="hero" sx={(theme) => ({width: '100%', backgroundRepeat: 'no-repeat', backgroundImage: 'radial-gradient(ellipse 80% 50% at 50% -20%, hsl(210, 100%, 90%), transparent)', ...theme.applyStyles('dark', {backgroundImage:'radial-gradient(ellipse 80% 50% at 50% -20%, hsl(210, 100%, 16%), transparent)'})})}>
